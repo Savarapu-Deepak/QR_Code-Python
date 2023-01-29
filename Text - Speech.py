6d0a7614865c077139e3e1d7aae3f2de19e5c8b9
Savarapu-Deepak <121797306+Savarapu-Deepak@users.noreply.github.com>
2023-01-29 22:47:23 +0530

    Delete Text - Speech.py

--- Text - Speech.py
+++
@@ -1,7 +0,0 @@
-# Python Program to Convert Text File to Speech File.
-# We use a module called gTTs.
-from gtts import gTTS  # Google text - Speech
-text = 'Hello Savarapu Deepak,How are You and welcome to our company as a Python Developer.'
-language = 'en'
-obj = gTTS(text=text, lang=language, slow=False)
-obj.save('gTTs.mp3')
